94f1ff15d5808915f8345974673722fd6e11dcbc
LukaTabagari2222 <tabagariluka32@gmail.com>
2024-07-09 14:10:45 +0400

    js rtuli davamte chemit

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,40 @@ let burgea = document.getElementById('burgerIa');
  burgea.addEventListener('click', function(){
     navigeishen.classList.add('neewigeishen')
  })
+
+
+
+ const vinoEnglish = document.querySelector('.vino');
+
+ vinoEnglish.addEventListener('mouseover', function () {
+   vinoEnglish.textContent = "ღვინო";
+ });
+
+ vinoEnglish.addEventListener('mouseout', function () {
+   vinoEnglish.textContent = "VINO";
+ });
+
+
+
+
+ const vinoSpanish = document.querySelector('.lamejor');
+
+ vinoSpanish.addEventListener('mouseover', function () {
+   vinoSpanish.textContent = "ღვინოებსი საუკეთესო არჩევანი";
+ });
+
+ vinoSpanish.addEventListener('mouseout', function () {
+   vinoSpanish.textContent = "LA MEJOR SELECCION DE VINOS";
+ });
+ 
+
+ const descrubeFrench = document.querySelector('.descrube');
+
+ descrubeFrench.addEventListener('mouseover', function () {
+   descrubeFrench.textContent = "ღვინოებსი საუკეთესო არჩევანი!";
+ });
+
+ descrubeFrench.addEventListener('mouseout', function () {
+   descrubeFrench.textContent = "DESCUBRE NUESTRO CATALOGO COMPLETO";
+ });
+
